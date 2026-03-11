5a9a4051a3e56a5c218895a7d5f1e96e834c2cfa
lorenadle <DOBRICALORENA12@stud.ase.ro>
2026-03-11 13:51:01 +0200

    add activitate

---
+++ Intalnire21Februrie_LSI.c
@@ -0,0 +1,124 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//trebuie sa folositi fisierul masini.txt
+//sau va creati un alt fisier cu alte date
+
+struct StructuraMasina {
+	int id;
+	int nrUsi;
+	float pret;
+	char* model;
+	char* numeSofer;
+	unsigned char serie;
+};
+typedef struct StructuraMasina Masina;
+typedef struct Nod Nod;
+
+struct Nod {
+	Masina info;
+	Nod* next;
+};
+
+Masina citireMasinaDinFisier(FILE* file) {
+	char buffer[100];
+	char sep[3] = ",\n";
+	fgets(buffer, 100, file);
+	char* aux;
+	Masina m1;
+	aux = strtok(buffer, sep);
+	m1.id = atoi(aux);
+	m1.nrUsi = atoi(strtok(NULL, sep));
+	m1.pret= atof(strtok(NULL, sep));
+	aux = strtok(NULL, sep);
+	m1.model = malloc(strlen(aux) + 1);
+	strcpy_s(m1.model, strlen(aux) + 1, aux);
+
+	aux = strtok(NULL, sep);
+	m1.numeSofer = malloc(strlen(aux) + 1);
+	strcpy_s(m1.numeSofer, strlen(aux) + 1, aux);
+
+	m1.serie = *strtok(NULL, sep);
+	return m1;
+}
+
+void afisareMasina(Masina masina) {
+	printf("Id: %d\n", masina.id);
+	printf("Nr. usi : %d\n", masina.nrUsi);
+	printf("Pret: %.2f\n", masina.pret);
+	printf("Model: %s\n", masina.model);
+	printf("Nume sofer: %s\n", masina.numeSofer);
+	printf("Serie: %c\n\n", masina.serie);
+}
+
+void adaugaMasinaInLista(Nod* *lista, Masina masinaNoua) {
+	Nod *nou= malloc(sizeof(Nod));
+	nou->info = masinaNoua; //ne asumam ca realizam shallow copy
+	nou->next = NULL;
+
+	if (*lista==NULL) {
+		*lista = nou;
+	}
+	else {
+		Nod* p = *lista;
+		while (p->next) {
+			p = p->next;
+		}
+		p->next = nou;
+	}
+}
+
+void adaugaLaInceputInLista(/*lista de masini*/ Masina masinaNoua) {
+	//adauga la inceputul listei o noua masina pe care o primim ca parametru
+}
+
+void afisareListaMasini(Nod* lista) {
+	while (lista) {
+		afisareMasina(lista->info);
+		lista = lista->next;
+	}
+}
+Nod* citireListaMasiniDinFisier(const char* numeFisier) {
+	FILE* f = fopen(numeFisier, "r");
+	Nod* lista = NULL;
+	while (!feof(f)) {
+		Masina m = citireMasinaDinFisier(f);
+		adaugaMasinaInLista(&lista, m);
+	}
+	fclose(f);
+	return lista;
+}
+
+void dezalocareListaMasini(Nod** lista) {
+	while (*lista) {
+		free((*lista)->info.model);
+		free((*lista)->info.numeSofer);
+		Nod* p = *lista;
+		*lista = (*lista)->next;
+		free(p);
+	}
+}
+
+float calculeazaPretMediu(/*lista de masini*/) {
+	//calculeaza pretul mediu al masinilor din lista.
+	return 0;
+}
+
+void stergeMasiniDinSeria(/*lista masini*/ char serieCautata) {
+	//sterge toate masinile din lista care au seria primita ca parametru.
+	//tratati situatia ca masina se afla si pe prima pozitie, si pe ultima pozitie
+}
+
+float calculeazaPretulMasinilorUnuiSofer(/*lista masini*/ const char* numeSofer) {
+	//calculeaza pretul tuturor masinilor unui sofer.
+	return 0;
+}
+
+int main() {
+	Nod* lista = citireListaMasiniDinFisier("masini2.txt");
+	afisareListaMasini(lista);
+	dezalocareListaMasini(&lista);
+	return 0;
+}
